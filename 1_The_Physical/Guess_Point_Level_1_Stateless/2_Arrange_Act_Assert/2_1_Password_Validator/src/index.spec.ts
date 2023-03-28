5581af0bac7c5cac4ecb0353481232bfc1da74a4
Hafeez Syed <feeziman007@hotmail.com>
2023-03-28 12:23:47 +1100

    [New]: initial commit

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,24 @@
 
 describe('password validator', () => {
+    test('password length should be between 5 and 15', () => {
+        const password = '12345678901234567890'
+        const result = passwordValidator(password)
+        expect(result).toBe(false)
+        
+    })
 
+    test('password should have at least 1 digit', () => {
+        const password = '12345678901234567890'
+        const result = passwordValidator(password)
+        expect(result).toBe(false)
+        
+    })
+
+    test('password should have at least 1 uppercase letter', () => {
+        const password = '12345678901234567890'
+        const result = passwordValidator(password)
+        expect(result).toBe(false)
+        
+    })
 })
 
