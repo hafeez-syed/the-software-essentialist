5171eefd84e7e0256a0cb217329f888957711dfd
Hafeez Syed <feeziman007@hotmail.com>
2023-03-29 11:36:00 +1100

    [New]: intial commit

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,8 @@
+const sequence: number[] = [2, 4, 21, -8, 53, 40]
 
 describe('stats calculator', () => {
-
-
+    test('return the minimum value from the sequence', () => {
+        const stats = new StatsCalculator(sequence)
+        expect(stats.minimumValue()).toBe(-8)
+    })
 })
