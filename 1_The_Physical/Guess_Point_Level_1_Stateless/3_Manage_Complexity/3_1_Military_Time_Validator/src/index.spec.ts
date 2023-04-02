a070739a037a1f87d3d97a99d04d5a234d7984ca
Hafeez Syed <feeziman007@hotmail.com>
2023-04-03 00:07:12 +1000

    [Update]: test to validate invalid time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,7 +2,7 @@ import MilitaryTime from './index'
 
 describe('military time validator', () => {
     let mTime
-    
+
     test('should return true for valid military time', () => {
         mTime = MilitaryTime('00:00')
         expect(mTime.isValid()).toBe(true)
@@ -12,5 +12,16 @@ describe('military time validator', () => {
 
         mTime = MilitaryTime('12:00')
         expect(mTime.isValid()).toBe(true)
+    })
+
+    test('should return false for invalid military time', () => {
+        mTime = MilitaryTime('24:00')
+        expect(mTime.isValid()).toBe(false)
+
+        mTime = MilitaryTime('00:60')
+        expect(mTime.isValid()).toBe(false)
+
+        mTime = MilitaryTime('25:60')
+        expect(mTime.isValid()).toBe(false)
     })
 })
