193c794c48ddc5acaf4d6eed179e9562e59c08a2
Hafeez Syed <feeziman007@hotmail.com>
2023-04-03 20:30:39 +1000

    [refactor]: refactor code to validate the false time range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,34 +5,48 @@ describe('military time validator', () => {
 
     test('should return true for valid military time', () => {
         mTime = new MilitaryTime('00:00')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
 
         mTime = new MilitaryTime('23:59')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
 
         mTime = new MilitaryTime('12:00')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
     })
 
     test('should return false for invalid military time', () => {
         mTime = new MilitaryTime('24:00')
-        expect(mTime.isValid()).toBe(false)
+        expect(mTime.isValid()).toBe('no')
 
         mTime = new MilitaryTime('00:60')
-        expect(mTime.isValid()).toBe(false)
+        expect(mTime.isValid()).toBe('no')
 
         mTime = new MilitaryTime('25:60')
-        expect(mTime.isValid()).toBe(false)
+        expect(mTime.isValid()).toBe('no')
     })
 
     test('should return true for valid time range', () => {
         mTime = new MilitaryTime('01:12 - 14:32')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
 
         mTime = new MilitaryTime('22:00 - 23:12')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
 
         mTime = new MilitaryTime('00:00 - 23:59')
-        expect(mTime.isValid()).toBe(true)
+        expect(mTime.isValid()).toBe('yes')
     })
+
+    test('should return false for invalid time range', () => {
+        mTime = new MilitaryTime('01:12 - 00:32')
+        expect(mTime.isValid()).toBe('no')
+
+        mTime = new MilitaryTime('22:00 - 21:12')
+        expect(mTime.isValid()).toBe('no')
+
+        mTime = new MilitaryTime('00:01 - 00:00')
+        expect(mTime.isValid()).toBe('no')
+
+        mTime = new MilitaryTime('25:00 - 12:23')
+        expect(mTime.isValid()).toBe('no')
+    })   
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,33 +2,38 @@ class MilitaryTime {
     constructor(private timeString: string) {
         this.timeString = timeString
     }
-    hasTimeRange() { 
+
+    private hasTimeRange() { 
         return this.timeString.includes(' - ')
     }
 
-    timeRange() {
+    private timeRange() {
         return this.timeString.split(' - ')
     }
 
-    validateSimpleTime(t: string) {
+    private validateSimpleTime(t: string) {
         const [hours, minutes] = t.split(':')
         const hoursNumber = Number(hours)
         const minutesNumber = Number(minutes)
         return hoursNumber >= 0 && hoursNumber <= 23 && minutesNumber >= 0 && minutesNumber <= 59
     }
 
-    validateTimeRange() {
+    private validateTimeRange() {
         const [startTime, endTime] = this.timeRange()
-        const [startHours] = startTime.split(':')
-        const [endHours] = endTime.split(':')
+        const [startHours, startMinutes] = startTime.split(':')
+        const [endHours, endMinutes] = endTime.split(':')
         const isStartTimeValid = this.validateSimpleTime(startTime)
         const isEndTimeValid = this.validateSimpleTime(endTime)
 
-        return isStartTimeValid && isEndTimeValid && startHours <= endHours
+        return isStartTimeValid && isEndTimeValid && startHours <= endHours  && startMinutes <= endMinutes
+    }
+
+    private convertBooleanToYesNo(bool: boolean) {
+        return bool ? 'yes' : 'no'
     }
 
     isValid() {
-        return this.hasTimeRange() ? this.validateTimeRange() : this.validateSimpleTime(this.timeString)
+        return this.hasTimeRange() ? this.convertBooleanToYesNo(this.validateTimeRange()) : this.convertBooleanToYesNo(this.validateSimpleTime(this.timeString))
     }    
 }
 
