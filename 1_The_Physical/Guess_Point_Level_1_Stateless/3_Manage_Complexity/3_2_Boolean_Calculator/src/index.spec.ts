5aa36e7118df396b5c0572a1e7bbba8833d8c158
Hafeez Syed <feeziman007@hotmail.com>
2023-04-03 22:06:47 +1000

    [New]: initial commit. implementing simple string to boolean conversion

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,20 @@
+import BooleanCalculator from './index'
+interface IBooleanCalculator {
+    setFirstNumber: (number: number) => void
+    setSecondNumber: (number: number) => void
+    setOperator: (op: string) => void
+    getResult: () => number
+    convert: (input: string) => boolean
+}
 
 describe('boolean calculator', () => {
+    let booleanCalculator: IBooleanCalculator
+    beforeEach(() => {
+        booleanCalculator = BooleanCalculator()
+    })
 
+    test('should read simple string and return boolean', () => {
+        expect(booleanCalculator.convert('TRUE')).toBe(true)
+        expect(booleanCalculator.convert('FALSE')).toBe(false)
+    })
 })
