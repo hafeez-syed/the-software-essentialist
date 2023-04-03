0c2e760064dee72755fbde645eb3f77e81f94196
Hafeez Syed <feeziman007@hotmail.com>
2023-04-03 20:17:39 +1000

    [refactor]: refactor code to validate the time range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -4,24 +4,35 @@ describe('military time validator', () => {
     let mTime
 
     test('should return true for valid military time', () => {
-        mTime = MilitaryTime('00:00')
+        mTime = new MilitaryTime('00:00')
         expect(mTime.isValid()).toBe(true)
 
-        mTime = MilitaryTime('23:59')
+        mTime = new MilitaryTime('23:59')
         expect(mTime.isValid()).toBe(true)
 
-        mTime = MilitaryTime('12:00')
+        mTime = new MilitaryTime('12:00')
         expect(mTime.isValid()).toBe(true)
     })
 
     test('should return false for invalid military time', () => {
-        mTime = MilitaryTime('24:00')
+        mTime = new MilitaryTime('24:00')
         expect(mTime.isValid()).toBe(false)
 
-        mTime = MilitaryTime('00:60')
+        mTime = new MilitaryTime('00:60')
         expect(mTime.isValid()).toBe(false)
 
-        mTime = MilitaryTime('25:60')
+        mTime = new MilitaryTime('25:60')
         expect(mTime.isValid()).toBe(false)
+    })
+
+    test('should return true for valid time range', () => {
+        mTime = new MilitaryTime('01:12 - 14:32')
+        expect(mTime.isValid()).toBe(true)
+
+        mTime = new MilitaryTime('22:00 - 23:12')
+        expect(mTime.isValid()).toBe(true)
+
+        mTime = new MilitaryTime('00:00 - 23:59')
+        expect(mTime.isValid()).toBe(true)
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,13 +1,35 @@
-function MilitaryTime(time: string) {
-    const [hours, minutes] = time.split(':')
-    
-    return {
-        isValid: () => {
-            const hoursNumber = Number(hours)
-            const minutesNumber = Number(minutes)
-            return hoursNumber >= 0 && hoursNumber <= 23 && minutesNumber >= 0 && minutesNumber <= 59
-        }    
+class MilitaryTime {
+    constructor(private timeString: string) {
+        this.timeString = timeString
     }
+    hasTimeRange() { 
+        return this.timeString.includes(' - ')
+    }
+
+    timeRange() {
+        return this.timeString.split(' - ')
+    }
+
+    validateSimpleTime(t: string) {
+        const [hours, minutes] = t.split(':')
+        const hoursNumber = Number(hours)
+        const minutesNumber = Number(minutes)
+        return hoursNumber >= 0 && hoursNumber <= 23 && minutesNumber >= 0 && minutesNumber <= 59
+    }
+
+    validateTimeRange() {
+        const [startTime, endTime] = this.timeRange()
+        const [startHours] = startTime.split(':')
+        const [endHours] = endTime.split(':')
+        const isStartTimeValid = this.validateSimpleTime(startTime)
+        const isEndTimeValid = this.validateSimpleTime(endTime)
+
+        return isStartTimeValid && isEndTimeValid && startHours <= endHours
+    }
+
+    isValid() {
+        return this.hasTimeRange() ? this.validateTimeRange() : this.validateSimpleTime(this.timeString)
+    }    
 }
 
 export default MilitaryTime
